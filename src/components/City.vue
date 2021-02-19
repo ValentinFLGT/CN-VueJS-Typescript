c09add6ebb8bb54f32e69ff657b52d8a80091188
ValentinFLGT <valentin.folliguet@le-campus-numerique.fr>
2021-02-19 10:12:53 +0100

    Resolve date display with moment plugin

--- src/components/City.vue
+++ src/components/City.vue
@@ -2,13 +2,14 @@
   <div>
     <h2>{{ name }}</h2>
     <p>Temps : {{ weather }} - Température : {{ temperature }}°C</p>
-    <p><em>Dernière mise à jour : {{ updatedAt }}</em></p>
+    <p><em>Dernière mise à jour : {{ currentDateTime() }}</em></p>
   </div>
 </template>
 
 <script lang="ts">
 
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
+import moment from 'moment';
 
 export default defineComponent({
   name: 'City',
@@ -17,7 +18,13 @@ export default defineComponent({
     weather: String,
     temperature: Number,
     updatedAt: Date
+  },
+  methods: {
+    currentDateTime() {
+      return moment(this.updatedAt).fromNow();
+    }
   }
+
 })
 
 

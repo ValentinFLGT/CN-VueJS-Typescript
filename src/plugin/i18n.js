adfed07acde24f37e7cc6391cb0c1c48ac80bd40
ValentinFLGT <valentin.folliguet@le-campus-numerique.fr>
2021-02-26 14:18:30 +0100

    Translations are in separate files

--- src/plugin/i18n.js
+++ src/plugin/i18n.js
@@ -1,18 +1,14 @@
 import { createI18n} from "vue-i18n";
+import en from "../assets/translations/translation.en"
+import fr from "../assets/translations/translation.fr"
 
 const messages = {
-    en: {
-        hi: "Hello!",
-        test: "Test",
-    },
-    fr: {
-        hi: "Bonjour !",
-        test: "Test",
-    },
+    fr,
+    en
 };
 
 const i18n = createI18n({
-    locale: "fr",
+    locale: "en",
     messages: messages,
 });
 
